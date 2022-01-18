2b9c8875915fa9e0b5d054ca798a9f0594a65fa6
nbc7799 <nbc7799@naver.com>
2022-01-18 22:58:20 +0900

    login

--- src/App.js
+++ src/App.js
@@ -4,9 +4,25 @@ import styles from "./app.module.css";
 function App() {
   const idRef = useRef();
   const passwordRef = useRef();
+  const savedUserName = localStorage.getItem("USERNAME_KEY");
+  const savedPassWord = localStorage.getItem("PASSWORD_KEY");
 
-  const onLogin = () => {};
+  const onLogin = (e) => {
+    e.preventDefault();
+    const username = idRef.current.value;
+    const password = passwordRef.current.value;
+    localStorage.setItem("USERNAME_KEY", username);
+    localStorage.setItem("PASSWORD_KEY", password);
+    doneLogin(username, password);
+  };
 
+  const doneLogin = (username, password) => {
+    if (savedUserName === "" || savedPassWord === "") {
+      alert("아이디와 비밀번호를 확인해주세요");
+    } else if (username === "박준규" && password === "123") {
+      alert("로그인 성공!");
+    }
+  };
   return (
     <section className={styles.loginBox}>
       <div className={styles.logo}>LOGO DESIGN</div>
